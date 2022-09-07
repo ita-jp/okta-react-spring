dc3294ca25f1669d362e3b8a0a5855e05ac9fa06
ita-jp <tn00gm@gmail.com>
2022-09-07 21:59:07 +0900

    "Add a React GroupEdit component"
    https://developer.okta.com/blog/2022/06/17/simple-crud-react-and-spring-boot#add-a-react-groupedit-component

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import GroupList from './GroupList';
+import GroupEdit from './GroupEdit';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path='/groups' exact={true} element={<GroupList />} />
+        <Route path='/groups/:id' element={<GroupEdit />} />
       </Routes>
     </Router>
   )
